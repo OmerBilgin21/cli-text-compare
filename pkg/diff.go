b591ce202a5c5de1ea28922acffa904c5034b838
OmerBilgin21 <oemerbilgin@proton.me>
2026-04-05 12:08:13 +0200

    implement line level guard
    also migrate to diffing by bytes to avoid unnecessary string overhead

--- pkg/diff.go
+++ pkg/diff.go
@@ -1,9 +1,9 @@
 package pkg
 
 import (
+	"bytes"
 	"math"
 	"slices"
-	"strings"
 )
 
 type Matrix [][]int
@@ -38,7 +38,7 @@ func findSmallestResistance(i, j int, matrix Matrix) Action {
 	return ActionLeft
 }
 
-func calculateCost(i, j int, a, b []string, matrix Matrix) int {
+func calculateCost(i, j int, a, b []byte, matrix Matrix) int {
 	if a[i-1] == b[j-1] {
 		return matrix[i-1][j-1]
 	}
@@ -49,68 +49,69 @@ func calculateCost(i, j int, a, b []string, matrix Matrix) int {
 	return min(above, left, topLeft)
 }
 
-// func handleDelimiterReconciliation(i int, lineCount float64, actions *[]Action, actionToAdd Action) {
-// 	if i+1 != int(lineCount) {
-// 		*actions = append(*actions, actionToAdd)
-// 	}
-// }
+func reconcileTheLostDelimiter(inputOne, inputTwo [][]byte, separator []byte) ([][]byte, [][]byte) {
+	for i, bytesArr := range inputOne {
+		if len(inputOne)-1 != i {
+			inputOne[i] = append(bytesArr, separator...)
+		}
+	}
 
-// func Diff(strOne string, strTwo string) []Action {
-// 	linesOne, linesTwo := strings.Split(strOne, "\n"), strings.Split(strTwo, "\n")
-// 	arrCtr := math.Max(float64(len(linesOne)), float64(len(linesTwo)))
-//
-// 	var actions []Action
-//
-// 	for i := range int(arrCtr) {
-// 		if len(linesOne)-1 >= i && len(linesTwo)-1 >= i {
-// 			if linesOne[i] == linesTwo[i] {
-// 				// here, lines are the same, so we should just add the
-// 				// character count amount of "ActionMatch"s into the actions array
-// 				actions = append(actions, slices.Repeat([]Action{ActionMatch}, len(strings.Split(linesOne[i], "")))...)
-// 			} else {
-// 				actions = append(actions, levenshtein(linesOne[i], linesTwo[i])...)
-// 			}
-//
-// 			// we should add a ActionMatch to the lenght of the string because
-// 			// in go (and every programming language I know for that matter) when we split strings from a character or a set of characters
-// 			// the delimiter is excluded from the substrings (logically so), so we have effectively lost the new line from the end of every line
-// 			// and I would prefer to keep the texts 100% original solely due to a fear of side-effects later on
-// 			// this is true so long as we're not in the last line, last line just ends, does not require an addition
-// 			if linesOne[i] != "" {
-// 				handleDelimiterReconciliation(i, arrCtr, &actions, ActionMatch)
-// 			}
-// 		}
-//
-// 		if len(linesOne)-1 < i {
-// 			deletions := slices.Repeat([]Action{ActionDelete}, len(strings.Split(linesTwo[i], "")))
-// 			actions = append(actions, deletions...)
-//
-// 			if linesTwo[i] != "" {
-// 				// same delimiter reason, but here it's a deletion as the first line is empty
-// 				handleDelimiterReconciliation(i, arrCtr, &actions, ActionDelete)
-// 			}
-// 		}
-//
-// 		if len(linesTwo)-1 < i {
-// 			additions := slices.Repeat([]Action{ActionDelete}, len(strings.Split(linesOne[i], "")))
-// 			actions = append(actions, additions...)
-//
-// 			if linesOne[i] != "" {
-// 				// same delimiter reason, but here it's an addition as the second line is empty
-// 				handleDelimiterReconciliation(i, arrCtr, &actions, ActionInsert)
-// 			}
-// 		}
-//
-// 	}
-//
-// 	return actions
-// }
+	for i, bytesArr := range inputTwo {
+		if len(inputTwo)-1 != i {
+			inputTwo[i] = append(bytesArr, separator...)
+		}
+	}
+
+	return inputOne, inputTwo
+}
+
+func Diff(oldInput []byte, newInput []byte) []Action {
+	separator := []byte("\n")
+	oldLines, newLines := bytes.SplitAfter(oldInput, separator), bytes.SplitAfter(newInput, separator)
+	arrCtr := math.Max(float64(len(oldLines)), float64(len(newLines)))
+
+	var actions []Action
+
+	for i := range int(arrCtr) {
+		// this loop one and below are basically us running out of one of the lines arrays
+		// so depending on which one, it's just a straight up addition/removal until longer ends
+		if len(oldLines)-1 < i {
+			for j := i; j < len(newLines); j++ {
+				for range len(newLines[j]) {
+					actions = append(actions, ActionInsert)
+				}
+			}
+
+			break
+		}
+
+		if len(newLines)-1 < i {
+			for j := i; j < len(oldLines); j++ {
+				for range len(oldLines[j]) {
+					actions = append(actions, ActionDelete)
+				}
+			}
+			break
+		}
+
+		if slices.Equal(oldLines[i], newLines[i]) {
+			// here, lines are the same, so we should just add the
+			// character count amount of "ActionMatch"s into the actions array
+			actions = append(actions, slices.Repeat([]Action{ActionMatch}, len(oldLines[i]))...)
+		} else {
+			actions = append(actions, levenshtein(oldLines[i], newLines[i])...)
+		}
+
+	}
+
+	return actions
+}
 
 // implemented by looking at: https://gist.github.com/jasonm23/449e7c572b46942361bc808357019dda
 // though jasonm23 refers to this as the "myers DIFF Algorithm" it seemed more like
 // The levenshtein Algorithm to me
-func Diff(strOne string, strTwo string) []Action {
-	a, b := strings.Split(strOne, ""), strings.Split(strTwo, "")
+func levenshtein(a []byte, b []byte) []Action {
+	// a, b := strings.Split(strOne, ""), strings.Split(strTwo, "")
 	m, n := len(a), len(b)
 
 	matrix := make(Matrix, m+1)
